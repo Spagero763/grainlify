67c9d87712898d12f2362a92eb45621a45b57d36
Mister-Money <moneyseason000@gmail.com>
2026-04-27 05:55:45 +0200

    Fixed Issue #1071

--- contracts/program-escrow/src/lib.rs
+++ contracts/program-escrow/src/lib.rs
@@ -2041,6 +2041,8 @@ impl ProgramEscrowContract {
                 return Err(BatchError::InvalidBatchSizeProgram);
             }
 
+            Self::enforce_token_allowlist(&env, &token_address, &program_id);
+
             let program_data = ProgramData {
                 program_id: program_id.clone(),
                 total_funds: 0,
